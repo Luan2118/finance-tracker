dd33abb12cf92e137ce670790198f906065dd6a5
Luan2118 <leluanvn21@seznam.cz>
2025-05-24 17:26:44 +0200

    make main page to show only limited number of submits

--- script/main.js
+++ script/main.js
@@ -9,45 +9,44 @@ displayIncome();
 
 function displayRecentTransactions() {
   let sharedDataHTML = '';
-  
-  sharedData.forEach((dataObject) => {
-    if (dataObject.type === 'expense') {
+
+  for (let i = 0 ; i < sharedData.length && i < 10;  i++) {
+    if (sharedData[i].type === 'expense') {
       const html = `
         <div class="transactions-info-inner-grid">
           <div class="transaction-img-grid"><img class="transactions-img" src="icons/shopping-icon.png"></div>
           <div class="transactions-info">
             <div>
               <div>
-              ${dataObject.expenseSourceValue}
+              ${sharedData[i].expenseSourceValue}
               </div>
-              <div class="transactions-date">${dataObject.dateValue}</div>
+              <div class="transactions-date">${sharedData[i].dateValue}</div>
             </div>
             
-            <div class="transactions-amount-minus">-${dataObject.amountValue}Kč</div>
+            <div class="transactions-amount-minus">-${sharedData[i].amountValue}Kč</div>
           </div>
         </div>
       `
       sharedDataHTML += html
-    }else if (dataObject.type === 'income') {
+
+    }else if (sharedData[i].type === 'income') {
       const html = `
         <div class="transactions-info-inner-grid">
           <div class="transaction-img-grid"><img class="transactions-img" src="icons/salary-icon.png"></div>
           <div class="transactions-info">
             <div>
-              <div>${dataObject.incomeSourceValue}</div>
-              <div class="transactions-date">${dataObject.dateValue}</div>
+              <div>${sharedData[i].incomeSourceValue}</div>
+              <div class="transactions-date">${sharedData[i].dateValue}</div>
             </div>
             
-            <div class="transactions-amount-plus">+${dataObject.amountValue}Kč</div>
+            <div class="transactions-amount-plus">+${sharedData[i].amountValue}Kč</div>
           </div>
         </div>
       `
       sharedDataHTML += html
     }
-
+  }
   
-  });
-
   document.querySelector('.js-transactions-info-grid')
     .innerHTML = sharedDataHTML;
 }
@@ -55,23 +54,23 @@ function displayRecentTransactions() {
 function displayExpenses() {
   let expenseDataHTML = '';
 
-  expenseData.forEach((dataObject) => {
+  for (let i = 0 ; i < expenseData.length && i < 5 ; i ++) {
     const html = `
       <div class="transactions-info-inner-grid">
         <div class="transaction-img-grid"><img class="transactions-img" src="icons/shopping-icon.png"></div>
         <div class="transactions-info">
           <div>
-            <div>${dataObject.expenseSourceValue}</div>
-            <div class="transactions-date">${dataObject.dateValue}</div>
+            <div>${expenseData[i].expenseSourceValue}</div>
+            <div class="transactions-date">${expenseData[i].dateValue}</div>
           </div>
           
-          <div class="transactions-amount-minus">-${dataObject.amountValue}Kč</div>
+          <div class="transactions-amount-minus">-${expenseData[i].amountValue}Kč</div>
         </div>
       </div>  
     `
 
     expenseDataHTML += html;
-  })
+  }
 
   document.querySelector('.js-expense-transactions-info-grid').innerHTML = expenseDataHTML;
 
@@ -81,22 +80,31 @@ function displayExpenses() {
 function displayIncome() {
   let incomeDataHTML = '';
 
-  incomeData.forEach((dataObject) => {
-    let html = `
+  for (let i = 0 ; i < incomeData.length && i < 5 ; i ++ ) {
+     let html = `
       <div class="transactions-info-inner-grid">
         <div class="transaction-img-grid"><img class="transactions-img" src="icons/shopping-icon.png"></div>
         <div class="transactions-info">
           <div>
-            <div>${dataObject.incomeSourceValue}</div>
-            <div class="transactions-date">${dataObject.dateValue}</div>
+            <div>${incomeData[i].incomeSourceValue}</div>
+            <div class="transactions-date">${incomeData[i].dateValue}</div>
           </div>
           
-          <div class="transactions-amount-plus">+${dataObject.amountValue}Kč</div>
+          <div class="transactions-amount-plus">+${incomeData[i].amountValue}Kč</div>
         </div>
       </div>
     `
     incomeDataHTML += html
-  })
-
+  }
   document.querySelector('.js-income-transactions-info-grid').innerHTML = incomeDataHTML;
 }
+
+
+const num1 = 1;
+const num2 = '1';
+
+const result = num1 === num2;
+
+const num3 = 'luan';
+
+console.log(result);
