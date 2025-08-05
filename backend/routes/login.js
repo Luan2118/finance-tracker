ab1365101435dd0ba576ff8a78f24d20f9573610
Luan2118 <leluanvn21@seznam.cz>
2025-08-05 18:35:09 +0200

    created authenticateToken middleware

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -3,15 +3,25 @@ import User from '../models/user.js'
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
+
 const router = express.Router();
 
+router.get('/user', authenticateToken, async(req, res, next) => {
+  try {
+    const user = await User.findOne({username: req.user.username});
+
+    res.send(user)
+  } catch (error) {
+    next(error)
+  }
+})
 
 
 router.post('/', async (req, res, next) => {  
   try {
     const {email, password} = req.body;
     
-    const user = await User.find({email: email})
+    const user = await User.findOne({email: email})
 
     if(user.length === 0) {
       const error = new Error('User not found')
@@ -21,7 +31,7 @@ router.post('/', async (req, res, next) => {
 
 
 
-    const isMatch = await bcrypt.compare(password, user[0].password);
+    const isMatch = await bcrypt.compare(password, user.password);
 
     if(!isMatch) {
       const error = new Error('Invalid password')
@@ -30,12 +40,21 @@ router.post('/', async (req, res, next) => {
     }
 
     const payload = {
-      id: user[0]._id,
-      username: user[0].username
+      id: user._id,
+      username: user.username
     }
 
-    const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET)
+    const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: '15m'
+    })
     const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET)
+
+    res.cookie('refreshToken', refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'Strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000
+    });
 
     res.status(200).json({
       accessToken,
@@ -47,7 +66,32 @@ router.post('/', async (req, res, next) => {
 })
 
 
+// router.get('/refresh', (req, res) => {
 
+// })
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    const error = new Error('Invalid token')
+    error.status = 400;
+    return next(error)
+  }
+
+  console.log(token)
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
+    if(err) {
+      const error = new Error('Token veritification failed')
+      error.status = 400;
+      return next(error)
+    }
+
+    req.user = payload;
+    next()
+  })
+}
 
 
 export default router;
