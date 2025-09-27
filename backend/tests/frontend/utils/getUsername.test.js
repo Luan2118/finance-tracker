edcd975623987cd6c744c56edbb5791b9d42f748
Luan <leluanvn21@seznam.cz>
2025-09-27 11:59:00 +0200

    test(getUsername): move vi.mock calls to top-level and add afterEach cleanup

--- backend/tests/frontend/utils/getUsername.test.js
+++ backend/tests/frontend/utils/getUsername.test.js
@@ -1,25 +1,26 @@
 import {describe, it, expect, vi} from 'vitest';
-import getAccessToken from '../../../../frontend/script/utils/getAccessToken';
-import refreshToken from '../../../../frontend/script/utils/refreshToken';
 import getUsername from '../../../../frontend/script/utils/getUsername';
 
+
+vi.mock('../../../../frontend/script/utils/getAccessToken', () => {
+  return {
+    default: vi.fn().mockReturnValue('FAKE_ACCESS_TOKEN')
+  }
+})
+
+
+vi.mock('../../../../frontend/script/utils/refreshToken', () => {
+  return {
+    default: vi.fn().mockResolvedValue('FAKE_REFRESH_TOKEN')
+  }
+})
+
+afterEach(() => {
+  vi.clearAllMocks();
+})
+
 describe('getUsername', () => {
-  it('happy road', async () => {
-
-    vi.mock('../../../../frontend/script/utils/getAccessToken', () => {
-      return {
-        default: vi.fn().mockReturnValue('FAKE_ACCESS_TOKEN')
-      }
-    })
-
-
-    vi.mock('../../../../frontend/script/utils/refreshToken', () => {
-      return {
-        default: vi.fn().mockResolvedValue('FAKE_REFRESH_TOKEN')
-      }
-    })
-
- 
+  it('happy path', async () => {
     const mockResponse = {
       username: 'Luan'
     }
@@ -30,7 +31,6 @@ describe('getUsername', () => {
       json: () => Promise.resolve(mockResponse)
       })
     })
-
 
     expect(await getUsername()).toBe('Luan')
 
